923a9080eb7c50d65c6daff68d2d3929c5530acf
Hassan-Mallah <58323397+Hassan-Mallah@users.noreply.github.com>
2020-04-15 14:38:06 +0300

    Update tests.py

--- Contacts/tests.py
+++ Contacts/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from . import views
+from django.urls import reverse, resolve
 
 # Create your tests here.
+
+
+class TestUrls(SimpleTestCase):
+    def test_home_url_resolves(self):
+        url = reverse('home')
+        self.assertEqual(resolve(url).func, views.home)
